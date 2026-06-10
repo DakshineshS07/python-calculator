8b376c0f25e2e7f3cecc9dec6b32236276b6995c
DakshineshS07 <sdakshinesh07@gmail.com>
2026-06-10 21:07:57 +0530

    Refactor calculator to handle zero division error
    Added custom exception handling for division by zero.

--- calc.py
+++ calc.py
@@ -1,6 +1,8 @@
+class MyZeroDivisionError(Exception):
+    pass
 def calculator():
-    num1=int(input("enter 1st number:"))
-    num2=int(input("enter 2nd number:"))
+    num1=float(input("enter 1st number:"))
+    num2=float(input("enter 2nd number:"))
     print("1.addition")
     print("2.subtraction")
     print("3.multiplication")
@@ -15,8 +17,17 @@ def calculator():
     elif(choice==3):
         mul=num1*num2
         print("multiplication:",mul)
+    elif(choice==4):
+        try:
+            if(num2==0):
+                raise MyZeroDivisionError
+            else:
+                div=num1/num2
+                print("division:",div)
+        except MyZeroDivisionError:
+            print("zero cannot be divided")
     else:
-        div=num1/num2
-        print("division:",div)
+        print("invalid input")
 calculator()
         
+
